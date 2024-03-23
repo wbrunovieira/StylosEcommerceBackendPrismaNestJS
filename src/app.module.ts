67a8ccfd37b3482ed9ec1f187aedfe0f35f4e3bf
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-23 11:37:36 -0300

    auth config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from 'src/env';
@@ -10,6 +11,7 @@ import { envSchema } from 'src/env';
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
