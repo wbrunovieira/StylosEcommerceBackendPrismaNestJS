99e67ab9a534939c57a1f0063781ba00aa10f137
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-23 15:54:19 -0300

    created auth decorator

--- src/controllers/create-products.controller.ts
+++ src/controllers/create-products.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user-decorator';
+import { UserPayload } from 'src/auth/jwt.strategy';
 
 @Controller('/products')
 @UseGuards(JwtAuthGuard)
@@ -7,7 +9,7 @@ export class CreateProductController {
   constructor() {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload){
     return 'ok';
   }
 }
