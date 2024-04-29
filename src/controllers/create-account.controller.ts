09df8dd2123a1d3861aac4daa28319406d519fbc
wbrunovieira <wbrunovieira77@gmail.com>
2024-04-29 12:34:03 -0300

    added new route update user

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,10 +1,13 @@
 import {
   ConflictException,
   Delete,
-  Get,
   HttpException,
   HttpStatus,
+  NotFoundException,
   Param,
+  Patch,
+  Put,
+  UseGuards,
 } from '@nestjs/common';
 import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -12,6 +15,7 @@ import { hash } from 'bcryptjs';
 import { z } from 'zod';
 import { ZodValidationsPipe } from 'src/pipes/zod-validations-pipe';
 import { JwtService } from '@nestjs/jwt';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -25,6 +29,14 @@ const createGoogleAccountBodySchema = z.object({
   googleUserId: z.string(),
   profileImageUrl: z.string(),
 });
+
+const updateUserBodySchema = z.object({
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z.string().min(6).optional(),
+});
+
+type UpdateUserBodySchema = z.infer<typeof updateUserBodySchema>;
 
 type CreateGoogleAccountBodySchema = z.infer<
   typeof createGoogleAccountBodySchema
@@ -151,5 +163,41 @@ export class CreateAccountController {
     }
 
     return false;
+  }
+
+  @Put('/:id')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async updateUser(@Param('id') id: string, @Body() body: any) {
+    const { name, email, password } = body;
+    console.log('Rota updateUser foi acessada');
+    console.log('atualizando usuario', body, id);
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const updatedUserData: any = {};
+
+    if (name !== undefined) updatedUserData.name = name;
+    if (email !== undefined) updatedUserData.email = email;
+    if (password !== undefined) {
+      const hashPassword = await hash(password, 8);
+      updatedUserData.password = hashPassword;
+    }
+
+    const updatedUser = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updatedUserData,
+    });
+
+    return updatedUser;
   }
 }
