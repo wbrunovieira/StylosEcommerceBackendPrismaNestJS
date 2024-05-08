40907b1dc48996561b66f2121d1be4e52c054cc4
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-08 15:24:04 -0300

    delete category usecase and unit test

---
+++ test/factories/make-category.ts
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker";
+
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+import {
+  Category,
+  CategoryProps,
+} from "@/domain/catalog/enterprise/entities/category";
+
+export function makeCategory(
+  override: Partial<CategoryProps> = {},
+  id?: UniqueEntityID
+) {
+  const category = Category.create(
+    {
+      name: faker.commerce.productName(),
+
+      ...override,
+    },
+    id
+  );
+
+  return category;
+}
