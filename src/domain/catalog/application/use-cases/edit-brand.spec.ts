ebbc5eac16acf2b3f1c42297efa005f2f28e3c6c
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-16 08:04:41 -0300

    edit brand unit test improved

--- src/domain/catalog/application/use-cases/edit-brand.spec.ts
+++ src/domain/catalog/application/use-cases/edit-brand.spec.ts
@@ -13,17 +13,22 @@ describe("Edit Brand", () => {
     sut = new EditBrandUseCase(inMemoryBrandsRepository as any);
   });
 
-  it("should be able to edit a brand", async () => {
+  it("should be able to edit an existing  brand", async () => {
     const newBrand = makeBrand({}, new UniqueEntityID("brand-1"));
 
     await inMemoryBrandsRepository.create(newBrand);
 
     const result = await sut.execute({
       brandId: newBrand.id.toValue(),
-      name: "name teste",
+      name: "Updated Brand Name",
     });
 
     expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      const updatedBrand = result.value.brand;
+      expect(updatedBrand.id.toValue()).toBe("brand-1");
+      expect(updatedBrand.name).toBe("Updated Brand Name");
+    }
   });
 
   it("should return an error if the brand does not exist", async () => {
@@ -32,7 +37,29 @@ describe("Edit Brand", () => {
       name: "name teste",
     });
 
-    expect(result.isLeft()).toBe(true);
-    expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+    if (result.isLeft()) {
+      const error = result.value as ResourceNotFoundError;
+      expect(error).toBeInstanceOf(ResourceNotFoundError);
+      expect(error.message).toBe("Brand not found");
+    }
+  });
+
+  it("should not change the brand if the name is the same", async () => {
+    const brandId = new UniqueEntityID("brand-3");
+    const newBrand = makeBrand({ name: "Same Brand Name" }, brandId);
+
+    await inMemoryBrandsRepository.create(newBrand);
+
+    const result = await sut.execute({
+      brandId: brandId.toValue(),
+      name: "Same Brand Name",
+    });
+
+    expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      const updatedBrand = result.value.brand;
+      expect(updatedBrand.id.toValue()).toBe("brand-3");
+      expect(updatedBrand.name).toBe("Same Brand Name");
+    }
   });
 });
