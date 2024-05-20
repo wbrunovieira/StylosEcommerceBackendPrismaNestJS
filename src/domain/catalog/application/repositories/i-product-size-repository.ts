5968f2aa0486a6d56f73d1061f47a1490d617b5a
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-20 17:48:51 -0300

    start test for create product with sizes

--- src/domain/catalog/application/repositories/i-product-size-repository.ts
+++ src/domain/catalog/application/repositories/i-product-size-repository.ts
@@ -1,10 +1,12 @@
+import { ProductSize } from "../../enterprise/entities/product-size";
+
 export interface IProductSizeRepository {
   create(productId: string, sizeId: string): Promise<void>;
-  // findByProductId(productId: string): Promise<ProductSize[]>;
-  // findBySizeId(
-  //   sizeId: string,
-  //   params: PaginationParams
-  // ): Promise<ProductSize[]>;
+  findByProductId(productId: string): Promise<ProductSize[]>;
+   findBySizeId(
+    sizeId: string,
+   
+  ): Promise<ProductSize[]>;
   // delete(productSize: ProductSize): Promise<void>;
   // deleteAllByProductId(productId: string): Promise<void>;
 }
