83b67f1eda5a0516ad88d711c46232057ae2f459
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-22 12:35:07 -0300

    added VAriant chema and new fields in Product schema

--- prisma/migrations/20240522145434_init/migration.sql
+++ prisma/migrations/20240522145434_init/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "ProductStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'OUT_OF_STOCK');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
@@ -29,6 +32,14 @@ CREATE TABLE "products" (
     "width" DOUBLE PRECISION,
     "length" DOUBLE PRECISION,
     "weight" DOUBLE PRECISION,
+    "sku" TEXT,
+    "upc" TEXT,
+    "tags" TEXT[],
+    "seoTitle" TEXT,
+    "seoDescription" TEXT,
+    "seoKeywords" TEXT,
+    "visibility" BOOLEAN NOT NULL DEFAULT true,
+    "status" "ProductStatus" NOT NULL DEFAULT 'ACTIVE',
     "onSale" BOOLEAN NOT NULL DEFAULT false,
     "isNew" BOOLEAN NOT NULL DEFAULT false,
     "isFeatured" BOOLEAN NOT NULL DEFAULT false,
@@ -38,6 +49,24 @@ CREATE TABLE "products" (
     "updatedAt" TIMESTAMP(3),
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_variants" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "colorId" TEXT,
+    "sizeId" TEXT,
+    "sku" TEXT NOT NULL,
+    "upc" TEXT,
+    "stock" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "images" TEXT[],
+    "status" "ProductStatus" NOT NULL DEFAULT 'ACTIVE',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "product_variants_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -137,11 +166,23 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "products_slug_key" ON "products"("slug");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "product_variants_sku_key" ON "product_variants"("sku");
+
 -- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_materialId_fkey" FOREIGN KEY ("materialId") REFERENCES "materials"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_brandId_fkey" FOREIGN KEY ("brandId") REFERENCES "brands"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variants" ADD CONSTRAINT "product_variants_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variants" ADD CONSTRAINT "product_variants_colorId_fkey" FOREIGN KEY ("colorId") REFERENCES "colors"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variants" ADD CONSTRAINT "product_variants_sizeId_fkey" FOREIGN KEY ("sizeId") REFERENCES "sizes"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "addresses" ADD CONSTRAINT "addresses_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
