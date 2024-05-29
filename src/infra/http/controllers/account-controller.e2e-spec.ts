dd7a67a537b0db777c10c81c0c7a4dfe8ad26066
bruno-minimac <wbrunovieira77@gmail.com>
2024-05-29 15:02:41 -0300

    create account e2e test

--- src/infra/http/controllers/account-controller.e2e-spec.ts
+++ src/infra/http/controllers/account-controller.e2e-spec.ts
@@ -1,10 +1,11 @@
-import { AppModule } from '@/app.module';
-import { PrismaService } from '@/prisma/prisma.service';
-import { INestApplication } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import request from 'supertest';
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
 
-describe('Create Account (E2E)', () => {
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+describe("Create Account (E2E)", () => {
   let app: INestApplication;
   let prisma: PrismaService;
 
@@ -15,26 +16,126 @@ describe('Create Account (E2E)', () => {
 
     app = moduleRef.createNestApplication();
 
-    prisma = moduleRef.get(PrismaService);
-
     await app.init();
+    prisma = app.get(PrismaService);
   });
 
-  test('[POST] /accounts', async () => {
-    const response = await request(app.getHttpServer()).post('/accounts').send({
-      name: 'Bruno Vieira',
-      email: 'bruno@example.com',
-      password: '123456',
+  afterAll(async () => {
+    await prisma.user.deleteMany({
+      where: {
+        email: {
+          in: ["bruno@example.com", "duplicate@example.com"],
+        },
+      },
+    });
+
+    await app.close();
+  });
+
+  test("[POST] /accounts  - Success", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "Bruno Vieira",
+      email: "bruno@example.com",
+      password: "12345@aA",
     });
 
     expect(response.statusCode).toBe(201);
 
     const userOnDatabase = await prisma.user.findUnique({
       where: {
-        email: 'bruno@example.com',
+        email: "bruno@example.com",
       },
     });
 
     expect(userOnDatabase).toBeTruthy();
+  });
+
+  test("[POST] /accounts - Missing Name", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      email: "missingname@example.com",
+      password: "12345@aA",
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toContain("Validation failed");
+    expect(response.body.errors.details).toContainEqual({
+      code: "invalid_type",
+      expected: "string",
+      message: "Required",
+      path: ["name"],
+      received: "undefined",
+    });
+  });
+
+  test("[POST] /accounts - Invalid Email", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "Invalid Email",
+      email: "invalid-email",
+      password: "12345@aA",
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toContain("Validation failed");
+    expect(response.body.errors.details).toContainEqual({
+      code: "invalid_string",
+      validation: "email",
+      message: "Invalid email",
+      path: ["email"],
+    });
+  });
+
+  test("[POST] /accounts - Weak Password", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "Weak Password",
+      email: "weakpassword@example.com",
+      password: "weak",
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toContain("Validation failed");
+    expect(response.body.errors.details).toContainEqual({
+      code: "too_small",
+      minimum: 6,
+      inclusive: true,
+      exact: false, 
+      message: "Password must be at least 6 characters long",
+      path: ["password"],
+      type: "string",
+    });
+    expect(response.body.errors.details).toContainEqual({
+      code: "invalid_string",
+      message: "Password must contain at least one uppercase letter",
+      path: ["password"],
+      validation: "regex",
+    });
+    expect(response.body.errors.details).toContainEqual({
+      code: "invalid_string",
+      message: "Password must contain at least one number",
+      path: ["password"],
+      validation: "regex",
+    });
+    expect(response.body.errors.details).toContainEqual({
+      code: "invalid_string",
+      message: "Password must contain at least one special character",
+      path: ["password"],
+      validation: "regex",
+    });
+  });
+
+  test("[POST] /accounts - Email Conflict", async () => {
+    await request(app.getHttpServer()).post("/accounts").send({
+      name: "Duplicate User",
+      email: "duplicate@example.com",
+      password: "12345@aA",
+    });
+
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "Duplicate User",
+      email: "duplicate@example.com",
+      password: "12345@aA",
+    });
+
+    expect(response.statusCode).toBe(409);
+    expect(response.body.message).toContain("User already exists");
   });
 });
