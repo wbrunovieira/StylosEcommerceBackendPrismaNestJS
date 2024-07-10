7737b90ef6f0086a588cd7dee3ce567c46b67617
bruno-minimac <wbrunovieira77@gmail.com>
2024-07-10 10:02:58 -0300

    added in product fields hasVariant and soft delete

--- src/domain/catalog/enterprise/entities/product.ts
+++ src/domain/catalog/enterprise/entities/product.ts
@@ -7,311 +7,316 @@ import dayjs from "dayjs";
 import { Entity } from "@/core/entities/entity";
 
 interface Category {
-  id: UniqueEntityID;
-  name: string;
+    id: UniqueEntityID;
+    name: string;
 }
 
 interface Brand {
-  id: UniqueEntityID;
-  name: string;
+    id: UniqueEntityID;
+    name: string;
 }
 
 interface Color {
-  id: UniqueEntityID;
-  name: string;
-  hex: string;
+    id: UniqueEntityID;
+    name: string;
+    hex: string;
 }
 interface Size {
-  id: UniqueEntityID;
-  name: string;
+    id: UniqueEntityID;
+    name: string;
 }
 
 export interface ProductProps {
-  name: string;
-  description: string;
-  productSizes?: Size[];
-  productColors?: Color[];
-  productCategories?: Category[];
+    name: string;
+    description: string;
+    productSizes?: Size[];
+    productColors?: Color[];
+    productCategories?: Category[];
 
-  materialId?: UniqueEntityID;
-  materialName?: string;
+    materialId?: UniqueEntityID;
+    materialName?: string;
 
-  sizeId?: UniqueEntityID[];
-  finalPrice?: number;
-  brandId: UniqueEntityID;
-  brandName?: string;
-  brandUrl?: string;
-  discount?: number;
-  price: number;
-  stock: number;
-  sku: string;
-  erpId?: string;
-  slug: Slug;
-  height: number;
-  width: number;
-  length: number;
-  weight: number;
-  onSale?: boolean;
-  isFeatured?: boolean;
-  isNew?: boolean;
-  images?: string[];
-  createdAt: Date;
-  updatedAt: Date;
+    sizeId?: UniqueEntityID[];
+    finalPrice?: number;
+    brandId: UniqueEntityID;
+    brandName?: string;
+    brandUrl?: string;
+    discount?: number;
+    price: number;
+    stock: number;
+    sku: string;
+    erpId?: string;
+    slug: Slug;
+    height: number;
+    width: number;
+    length: number;
+    weight: number;
+    onSale?: boolean;
+    isFeatured?: boolean;
+    isNew?: boolean;
+    images?: string[];
+    hasVariants: boolean;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 export class Product extends Entity<ProductProps> {
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  get height() {
-    return this.props.height;
-  }
-  get erpId(): string | undefined {
-    return this.props.erpId;
-  }
-
-  set height(height: number) {
-    this.props.height = height;
-    this.touch();
-  }
-  set erpId(erpId: string) {
-    this.props.erpId = erpId;
-    this.touch();
-  }
-
-  set width(width: number) {
-    this.props.width = width;
-    this.touch();
-  }
-  set length(length: number) {
-    this.props.length = length;
-    this.touch();
-  }
-
-  set weight(weight: number) {
-    this.props.weight = weight;
-    this.touch();
-  }
-  set onSale(onSale: boolean) {
-    this.props.onSale = onSale;
-    this.touch();
-  }
-  set isFeatured(isFeatured: boolean) {
-    this.props.isFeatured = isFeatured;
-    this.touch();
-  }
-  set isNew(isNew: boolean) {
-    this.props.isNew = isNew;
-    this.touch();
-  }
-  set images(images: string[]) {
-    this.props.images = images;
-    this.touch();
-  }
-
-  get sku() {
-    return this.props.sku;
-  }
-
-  get onSale() {
-    return this.props.onSale ?? false;
-  }
-  get discount() {
-    return this.props.discount;
-  }
-  get isFeatured() {
-    return this.props.isFeatured ?? false;
-  }
-  get images() {
-    return this.props.images ?? [];
-  }
-  get width() {
-    return this.props.width;
-  }
-  get length() {
-    return this.props.length;
-  }
-
-  get weight() {
-    return this.props.weight;
-  }
-  get name() {
-    return this.props.name;
-  }
-
-  get finalPrice() {
-    return this.props.finalPrice;
-  }
-
-  get description() {
-    return this.props.description;
-  }
-
-  get brandId() {
-    return this.props.brandId;
-  }
-
-  get materialId(): UniqueEntityID | undefined {
-    return this.props.materialId;
-  }
-
-  get sizeId(): UniqueEntityID[] | undefined {
-    return this.props.sizeId;
-  }
-
-  get price() {
-    return this.props.price;
-  }
-
-  get stock() {
-    return this.props.stock;
-  }
-
-  get slug() {
-    return this.props.slug;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  get isNew(): boolean {
-    return dayjs().diff(this.createdAt, "days") <= 30;
-  }
-
-  get excerpt() {
-    return this.description.substring(0, 120).trimEnd().concat("...");
-  }
-
-  set name(name: string) {
-    this.props.name = name;
-    this.props.slug = Slug.createFromText(name);
-
-    this.touch();
-  }
-
-  set description(description: string) {
-    this.props.description = description;
-
-    this.touch();
-  }
-
-  set brandId(brandId: UniqueEntityID) {
-    this.props.brandId = brandId;
-
-    this.touch();
-  }
-  set materialId(materialId: UniqueEntityID) {
-    this.props.materialId = materialId;
-
-    this.touch();
-  }
-
-  set sizeId(sizeId: UniqueEntityID[]) {
-    this.props.sizeId = sizeId;
-
-    this.touch();
-  }
-
-  set sku(sku: string) {
-    this.props.sku = sku;
-    this.touch();
-  }
-
-  set price(price: number) {
-    this.props.price = price;
-
-    this.touch();
-  }
-
-  set stock(stock: number) {
-    this.props.stock = stock;
-
-    this.touch();
-  }
-
-  set slug(slug: Slug) {
-    this.props.slug = slug;
-
-    this.touch();
-  }
-
-  get productSizes(): Size[] | undefined {
-    return this.props.productSizes;
-  }
-
-  set productSizes(sizes: Size[]) {
-    this.props.productSizes = sizes;
-    this.touch();
-  }
-
-  get productCategories(): Category[] | undefined {
-    return this.props.productCategories;
-  }
-
-  set productColors(colors: Color[]) {
-    this.props.productColors = colors;
-    this.touch();
-  }
-  set productCategories(categories: Category[]) {
-    this.props.productCategories = categories;
-    this.touch();
-  }
-
-  // addSize(sizeId: UniqueEntityID) {
-  //   if (!this.props.productSizes) {
-  //     this.props.productSizes = [];
-  //   }
-  //   this.props.productSizes.push(sizeId);
-  //   this.touch();
-  // }
-
-  // addColor(productColorsId: UniqueEntityID) {
-  //   if (!this.props.productColors) {
-  //     this.props.productColors = [];
-  //   }
-  //   this.props.productColors.push(productColorsId);
-  //   this.touch();
-  // }
-
-  addCategory(category: Category) {
-    if (!this.props.productCategories) {
-      this.props.productCategories = [];
+    private touch() {
+        this.props.updatedAt = new Date();
     }
-    this.props.productCategories.push(category);
-    this.touch();
-  }
 
-  set discount(discount: number | undefined) {
-    this.props.discount = discount;
-    this.touch();
-  }
+    get height() {
+        return this.props.height;
+    }
+    get erpId(): string | undefined {
+        return this.props.erpId;
+    }
 
-  setFinalPrice(finalPrice: number) {
-    this.props.finalPrice = finalPrice;
-    this.touch();
-  }
+    set height(height: number) {
+        this.props.height = height;
+        this.touch();
+    }
+    set erpId(erpId: string) {
+        this.props.erpId = erpId;
+        this.touch();
+    }
 
-  static create(
-    props: Optional<ProductProps, "createdAt" | "slug" | "updatedAt">,
-    id?: UniqueEntityID
-  ): Product {
-    const product = new Product(
-      {
-        ...props,
+    set width(width: number) {
+        this.props.width = width;
+        this.touch();
+    }
+    set length(length: number) {
+        this.props.length = length;
+        this.touch();
+    }
 
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        slug: props.slug ?? Slug.createFromText(props.name),
-      },
-      id
-    );
+    set weight(weight: number) {
+        this.props.weight = weight;
+        this.touch();
+    }
+    set onSale(onSale: boolean) {
+        this.props.onSale = onSale;
+        this.touch();
+    }
+    set hasVariants(hasVariants: boolean) {
+        this.props.hasVariants = hasVariants;
+        this.touch();
+    }
+    set isFeatured(isFeatured: boolean) {
+        this.props.isFeatured = isFeatured;
+        this.touch();
+    }
+    set isNew(isNew: boolean) {
+        this.props.isNew = isNew;
+        this.touch();
+    }
+    set images(images: string[]) {
+        this.props.images = images;
+        this.touch();
+    }
 
-    return product;
-  }
+    get sku() {
+        return this.props.sku;
+    }
+
+    get onSale() {
+        return this.props.onSale ?? false;
+    }
+    get discount() {
+        return this.props.discount;
+    }
+    get isFeatured() {
+        return this.props.isFeatured ?? false;
+    }
+    get images() {
+        return this.props.images ?? [];
+    }
+    get width() {
+        return this.props.width;
+    }
+    get length() {
+        return this.props.length;
+    }
+
+    get weight() {
+        return this.props.weight;
+    }
+    get name() {
+        return this.props.name;
+    }
+
+    get finalPrice() {
+        return this.props.finalPrice;
+    }
+
+    get description() {
+        return this.props.description;
+    }
+
+    get brandId() {
+        return this.props.brandId;
+    }
+
+    get materialId(): UniqueEntityID | undefined {
+        return this.props.materialId;
+    }
+
+    get sizeId(): UniqueEntityID[] | undefined {
+        return this.props.sizeId;
+    }
+
+    get price() {
+        return this.props.price;
+    }
+
+    get stock() {
+        return this.props.stock;
+    }
+
+    get slug() {
+        return this.props.slug;
+    }
+
+    get createdAt() {
+        return this.props.createdAt;
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt;
+    }
+
+    get isNew(): boolean {
+        return dayjs().diff(this.createdAt, "days") <= 30;
+    }
+
+    get excerpt() {
+        return this.description.substring(0, 120).trimEnd().concat("...");
+    }
+
+    set name(name: string) {
+        this.props.name = name;
+        this.props.slug = Slug.createFromText(name);
+
+        this.touch();
+    }
+
+    set description(description: string) {
+        this.props.description = description;
+
+        this.touch();
+    }
+
+    set brandId(brandId: UniqueEntityID) {
+        this.props.brandId = brandId;
+
+        this.touch();
+    }
+    set materialId(materialId: UniqueEntityID) {
+        this.props.materialId = materialId;
+
+        this.touch();
+    }
+
+    set sizeId(sizeId: UniqueEntityID[]) {
+        this.props.sizeId = sizeId;
+
+        this.touch();
+    }
+
+    set sku(sku: string) {
+        this.props.sku = sku;
+        this.touch();
+    }
+
+    set price(price: number) {
+        this.props.price = price;
+
+        this.touch();
+    }
+
+    set stock(stock: number) {
+        this.props.stock = stock;
+
+        this.touch();
+    }
+
+    set slug(slug: Slug) {
+        this.props.slug = slug;
+
+        this.touch();
+    }
+
+    get productSizes(): Size[] | undefined {
+        return this.props.productSizes;
+    }
+
+    set productSizes(sizes: Size[]) {
+        this.props.productSizes = sizes;
+        this.touch();
+    }
+
+    get productCategories(): Category[] | undefined {
+        return this.props.productCategories;
+    }
+
+    set productColors(colors: Color[]) {
+        this.props.productColors = colors;
+        this.touch();
+    }
+    set productCategories(categories: Category[]) {
+        this.props.productCategories = categories;
+        this.touch();
+    }
+
+    // addSize(sizeId: UniqueEntityID) {
+    //   if (!this.props.productSizes) {
+    //     this.props.productSizes = [];
+    //   }
+    //   this.props.productSizes.push(sizeId);
+    //   this.touch();
+    // }
+
+    // addColor(productColorsId: UniqueEntityID) {
+    //   if (!this.props.productColors) {
+    //     this.props.productColors = [];
+    //   }
+    //   this.props.productColors.push(productColorsId);
+    //   this.touch();
+    // }
+
+    addCategory(category: Category) {
+        if (!this.props.productCategories) {
+            this.props.productCategories = [];
+        }
+        this.props.productCategories.push(category);
+        this.touch();
+    }
+
+    set discount(discount: number | undefined) {
+        this.props.discount = discount;
+        this.touch();
+    }
+
+    setFinalPrice(finalPrice: number) {
+        this.props.finalPrice = finalPrice;
+        this.touch();
+    }
+
+    static create(
+        props: Optional<ProductProps, "createdAt" | "slug" | "updatedAt">,
+        id?: UniqueEntityID
+    ): Product {
+        const product = new Product(
+            {
+                ...props,
+
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                slug: props.slug ?? Slug.createFromText(props.name),
+            },
+            id
+        );
+
+        return product;
+    }
 }
--- src/domain/order/application/use-cases/create-cart.ts
+++ src/domain/order/application/use-cases/create-cart.ts
@@ -10,178 +10,211 @@ import { IProductRepository } from "@/domain/catalog/application/repositories/i-
 import { IProductVariantRepository } from "@/domain/catalog/application/repositories/i-product-variant-repository";
 
 interface CreateCartUseCaseRequest {
-  userId: string;
-  items: {
-    productId: string;
-    quantity: number;
-    price: number;
-    colorId?: string;
-    sizeId?: string;
-  }[];
+    userId: string;
+    items: {
+        productId: string;
+        quantity: number;
+        price: number;
+        colorId?: string;
+        sizeId?: string;
+    }[];
 }
 
 type CreateCartUseCaseResponse = Either<
-  ResourceNotFoundError | null,
-  {
-    cart: Cart;
-  }
+    ResourceNotFoundError | null,
+    {
+        cart: Cart;
+    }
 >;
 
 @Injectable()
 export class CreateCartUseCase {
-  constructor(
-    private cartRepository: ICartRepository,
-    private productRepository: IProductRepository,
-    private variantRepository: IProductVariantRepository 
-  ) {}
+    constructor(
+        private cartRepository: ICartRepository,
+        private productRepository: IProductRepository,
+        private variantRepository: IProductVariantRepository
+    ) {}
 
-  async execute({
-    userId,
-    items,
-  }: CreateCartUseCaseRequest): Promise<CreateCartUseCaseResponse> {
-    try {
-      
-      const cartItemsMap: { [productId: string]: CartItem } = {};
+    async execute({
+        userId,
+        items,
+    }: CreateCartUseCaseRequest): Promise<CreateCartUseCaseResponse> {
+        try {
+            const cartItemsMap: { [productId: string]: CartItem } = {};
 
-      for (const item of items) {
+            for (const item of items) {
+                if (item.quantity <= 0) {
+                    return left(
+                        new ResourceNotFoundError(
+                            "Quantity must be greater than zero"
+                        )
+                    );
+                }
 
-        if (item.quantity <= 0) {
-          return left(
-            new ResourceNotFoundError("Quantity must be greater than zero")
-          );
-        }
+                let productResult;
+                let variant;
 
-        let productResult;
-        let variant;
+                if (item.colorId && item.sizeId) {
+                    const variantResult = await this.variantRepository.findById(
+                        item.productId
+                    );
 
+                    console.log("variantResult", variantResult);
+                    console.log("variantResult.value;", variantResult.value);
 
-        if (item.colorId && item.sizeId) {
-          
-          const variantResult = await this.variantRepository.findById(
-            item.productId
-          );
+                    if (variantResult.isLeft()) {
+                        console.log("variantResult isLeft", variantResult);
+                        return left(
+                            new ResourceNotFoundError(
+                                `Variant not found: ${item.productId}`
+                            )
+                        );
+                    }
 
-          console.log('variantResult',variantResult)
-          console.log('variantResult.value;',variantResult.value)
-       
-          
-          if (variantResult.isLeft()) {
-            console.log('variantResult isLeft',variantResult)
-            return left(
-              new ResourceNotFoundError(`Variant not found: ${item.productId}`)
-            );
-          }
-          
-          variant = variantResult.value;
-          console.log('variant quase bom variantResult',variantResult)
+                    variant = variantResult.value;
+                    console.log(
+                        "variant quase bom variantResult",
+                        variantResult
+                    );
 
-          console.log('variant quase bom variant',variant)
-        
+                    console.log("variant quase bom variant", variant);
 
-          if (variant.stock < item.quantity) {
-            return left(
-              new ResourceNotFoundError(
-                `Insufficient stock for variant: ${item.productId}`
-              )
-            );
-          }
+                    if (variant.stock < item.quantity) {
+                        return left(
+                            new ResourceNotFoundError(
+                                `Insufficient stock for variant: ${item.productId}`
+                            )
+                        );
+                    }
 
-          console.log('produto com variant quase pronto:',variant)
-          
-          const productIdString = String(variant.props.productId.value)
-          
-          console.log('pid do produto da variant:',productIdString)
+                    console.log("produto com variant quase pronto:", variant);
 
-          productResult = await this.productRepository.findById(productIdString);
+                    const productIdString = String(
+                        variant.props.productId.value
+                    );
 
-          const { product, variants } = productResult.value;
+                    console.log("pid do produto da variant:", productIdString);
 
-          console.log('product consulltaaaa', product)
-          console.log('product consulltaaaa', variants)
+                    productResult =
+                        await this.productRepository.findById(productIdString);
 
-          const { height, width, length, weight } = product.props;
-          console.log(' product height height',product,height,width,length,weight)
+                    const { product, variants } = productResult.value;
 
-          if (cartItemsMap[item.productId]) {
-            console.log('item 11',item)
-            const existingItem = cartItemsMap[productIdString];
-            existingItem.setQuantity(existingItem.quantity + item.quantity);
-          } else {
-            console.log('item. ss',item)
-            cartItemsMap[productIdString] = new CartItem({
-              productId: item.productId,
-              quantity: item.quantity,
-              price: item.price,
-              height: height,
-              width: width,
-              length: length,
-              weight: weight,
-              color: item.colorId,
-              size: item.sizeId,
-            });
-          }
-        } else {
-          console.log('entrou no produto sem size e color com id ',item.productId)
-          productResult = await this.productRepository.findById(item.productId);
+                    console.log("product consulltaaaa", product);
+                    console.log("product consulltaaaa", variants);
 
-          const { product, variants } = productResult.value;
+                    const { height, width, length, weight } = product.props;
+                    console.log(
+                        " product height height",
+                        product,
+                        height,
+                        width,
+                        length,
+                        weight
+                    );
 
-            console.log('productResult product',product)
+                    if (cartItemsMap[productIdString]) {
+                        console.log("item 11", item);
+                        const existingItem = cartItemsMap[productIdString];
+                        existingItem.setQuantity(
+                            existingItem.quantity + item.quantity
+                        );
+                    } else {
+                        console.log("item. ss", item);
+                        cartItemsMap[productIdString] = new CartItem({
+                            productId: productIdString,
+                            quantity: item.quantity,
+                            price: item.price,
+                            height: height,
+                            width: width,
+                            length: length,
+                            weight: weight,
+                            color: item.colorId,
+                            size: item.sizeId,
+                        });
+                    }
+                } else {
+                    console.log(
+                        "entrou no produto sem size e color com id ",
+                        item.productId
+                    );
+                    productResult = await this.productRepository.findById(
+                        item.productId
+                    );
 
-            if (productResult.isLeft()) {
-              console.log('productResult product left 2',productResult)
-              return left(
-                new ResourceNotFoundError(`Product not found: ${item.productId}`)
-              );
+                    const { product, variants } = productResult.value;
+
+                    console.log("productResult product", product);
+
+                    if (productResult.isLeft()) {
+                        console.log(
+                            "productResult product left 2",
+                            productResult
+                        );
+                        return left(
+                            new ResourceNotFoundError(
+                                `Product not found: ${item.productId}`
+                            )
+                        );
+                    }
+
+                    console.log(" product", product);
+
+                    if (product.stock < item.quantity) {
+                        return left(
+                            new ResourceNotFoundError(
+                                `Insufficient stock for product: ${item.productId}`
+                            )
+                        );
+                    }
+
+                    const { height, width, length, weight } = product.props;
+                    console.log(
+                        " product height height",
+                        product,
+                        height,
+                        width,
+                        length,
+                        weight
+                    );
+
+                    if (cartItemsMap[item.productId]) {
+                        const existingItem = cartItemsMap[item.productId];
+                        existingItem.setQuantity(
+                            existingItem.quantity + item.quantity
+                        );
+                    } else {
+                        cartItemsMap[item.productId] = new CartItem({
+                            productId: item.productId,
+                            quantity: item.quantity,
+                            price: item.price,
+                            height,
+                            width,
+                            length,
+                            weight,
+                            color: item.colorId,
+                            size: item.sizeId,
+                        });
+                    }
+                }
+
+                console.log("chegou aqui cartItemsMap", cartItemsMap);
             }
-            
-            
-            console.log(' product',product)
-            
-            if (product.stock < item.quantity) {
-              return left(
-              new ResourceNotFoundError(
-                `Insufficient stock for product: ${item.productId}`
-              )
+            console.log(
+                "saiu do for no usecase com cartItemsMap",
+                cartItemsMap
             );
-          }
 
-          const { height, width, length, weight } = product.props;
-          console.log(' product height height',product,height,width,length,weight)
-          
-          if (cartItemsMap[item.productId]) {
-            const existingItem = cartItemsMap[item.productId];
-            existingItem.setQuantity(existingItem.quantity + item.quantity);
-          } else {
-            cartItemsMap[item.productId] = new CartItem({
-              productId: item.productId,
-              quantity: item.quantity,
-              price: item.price,
-              height,
-              width,
-              length,
-              weight,
-              color: item.colorId,
-              size: item.sizeId,
-            });
-          }
+            const cartItems = Object.values(cartItemsMap);
+            console.log("cartItems", cartItems);
+
+            const cart = Cart.create({ userId, items: cartItems });
+            console.log("cart in usecase", cart);
+            await this.cartRepository.create(cart);
+
+            return right({ cart });
+        } catch (error) {
+            return left(error as Error);
         }
-
-        console.log('chegou aqui cartItemsMap',cartItemsMap)
-
-      }
-      console.log('saiu do for no usecase com cartItemsMap',cartItemsMap)
-
-      const cartItems = Object.values(cartItemsMap);
-      console.log("cartItems", cartItems);
-
-      const cart = Cart.create({ userId, items: cartItems });
-      console.log("cart in usecase", cart);
-      await this.cartRepository.create(cart);
-
-      return right({ cart });
-    } catch (error) {
-      return left(error as Error);
     }
-  }
 }
