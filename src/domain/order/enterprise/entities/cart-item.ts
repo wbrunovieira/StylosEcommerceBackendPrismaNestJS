fa2cca9473ef6f64b31693eb9b9f2914a134183a
bruno-minimac <wbrunovieira77@gmail.com>
2024-07-11 10:50:33 -0300

    create cart with product no variant is working

--- src/domain/order/enterprise/entities/cart-item.ts
+++ src/domain/order/enterprise/entities/cart-item.ts
@@ -2,79 +2,82 @@ import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
 interface CartItemProps {
-  productId: string;
-  quantity: number;
-  price: number;
-  height: number;
-  width: number;
-  length: number;
-  weight: number;
-  color?: string;
-  size?: string;
+    productId: string;
+    quantity: number;
+    price: number;
+    height: number;
+    width: number;
+    length: number;
+    weight: number;
+    color?: string;
+    size?: string;
+    productIdVariant?: string;
 }
 
 export class CartItem extends Entity<CartItemProps> {
-  constructor(props: CartItemProps, id?: UniqueEntityID) {
-    super(props, id);
-  }
+    constructor(props: CartItemProps, id?: UniqueEntityID) {
+        super(props, id);
+    }
 
-  get productId(): string {
-    return this.props.productId;
-  }
-  get color(): string | undefined {
-    return this.props.color ;
-  }
-  get size(): string | undefined   {
-    return this.props.size;
-  }
+    get productId(): string {
+        return this.props.productId;
+    }
 
-  get quantity(): number {
-    return this.props.quantity;
-  }
+    get productIdVariant(): string | undefined {
+        return this.props.productIdVariant;
+    }
 
-  get price(): number {
-    return this.props.price;
-  }
-  get height(): number {
-    return this.props.height;
-  }
+    get color(): string | undefined {
+        return this.props.color;
+    }
+    get size(): string | undefined {
+        return this.props.size;
+    }
 
-  get width(): number {
-    return this.props.width;
-  }
+    get quantity(): number {
+        return this.props.quantity;
+    }
 
-  get length(): number {
-    return this.props.length;
-  }
+    get price(): number {
+        return this.props.price;
+    }
+    get height(): number {
+        return this.props.height;
+    }
 
-  get weight(): number {
-    return this.props.weight;
-  }
+    get width(): number {
+        return this.props.width;
+    }
 
-  setQuantity(quantity: number): void {
-    this.props.quantity = quantity;
-  }
+    get length(): number {
+        return this.props.length;
+    }
 
-  toObject() {
-    return {
-      id: this.id?.toString(),
-      productId: this.productId,
-      quantity: this.quantity,
-      price: this.price,
-      height: this.height,
-      width: this.width,
-      length: this.length,
-      weight: this.weight,
-      colorId: this.color,
-      sizeId: this.size,
-    };
-  }
+    get weight(): number {
+        return this.props.weight;
+    }
 
-  static create(props: CartItemProps, id?: UniqueEntityID): CartItem {
-    return new CartItem(props, id);
-  }
+    setQuantity(quantity: number): void {
+        this.props.quantity = quantity;
+    }
 
+    toObject() {
+        return {
+            id: this.id?.toString(),
+            productId: this.productId,
+            quantity: this.quantity,
+            price: this.price,
+            height: this.height,
+            width: this.width,
+            productIdVariant: this.productIdVariant,
+            length: this.length,
+            weight: this.weight,
+            colorId: this.color,
+            sizeId: this.size,
+        };
+    }
 
-
-
+    static create(props: CartItemProps, id?: UniqueEntityID): CartItem {
+        return new CartItem(props, id);
+    }
 }
--- src/infra/database/prisma/repositories/prisma-product-variant-repository.ts
+++ src/infra/database/prisma/repositories/prisma-product-variant-repository.ts
@@ -8,191 +8,209 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { ResourceNotFoundError } from "@/domain/catalog/application/use-cases/errors/resource-not-found-error";
 import { ProductStatus } from "@/domain/catalog/enterprise/entities/product-status";
 import {
-  toDomainProductStatus,
-  toPrismaProductStatus,
+    toDomainProductStatus,
+    toPrismaProductStatus,
 } from "../utils/convert-product-status";
 
 @Injectable()
 export class PrismaProductVariantRepository
-  implements IProductVariantRepository
-  
+    implements IProductVariantRepository
 {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
 
-  async create(productVariant: ProductVariant): Promise<Either<Error, void>> {
-    try {
-      await this.prisma.productVariant.create({
-        data: {
-          id: productVariant.id.toString(),
-          productId: productVariant.productId.toString(),
-          sizeId: productVariant.sizeId?.toString(),
-          colorId: productVariant.colorId?.toString(),
-          sku: productVariant.sku,
-          stock: productVariant.stock,
-          price: productVariant.price,
-          status: toPrismaProductStatus(productVariant.status),
-          images: productVariant.images,
-          createdAt: productVariant.createdAt,
-          updatedAt: productVariant.updatedAt,
-        },
-      });
-      return right(undefined);
-    } catch (error) {
-      return left(new Error("Failed to create product variant"));
-    }
-  }
-
-  async findByProductId(
-    productId: string
-  ): Promise<Either<ResourceNotFoundError, ProductVariant[]>> {
-    const variants = await this.prisma.productVariant.findMany({
-      where: { productId },
-    });
-
-    if (variants.length === 0) {
-      return left(
-        new ResourceNotFoundError(
-          `Variants not found for product id: ${productId}`
-        )
-      );
+    async create(productVariant: ProductVariant): Promise<Either<Error, void>> {
+        try {
+            await this.prisma.productVariant.create({
+                data: {
+                    id: productVariant.id.toString(),
+                    productId: productVariant.productId.toString(),
+                    sizeId: productVariant.sizeId?.toString(),
+                    colorId: productVariant.colorId?.toString(),
+                    sku: productVariant.sku,
+                    stock: productVariant.stock,
+                    price: productVariant.price,
+                    status: toPrismaProductStatus(productVariant.status),
+                    images: productVariant.images,
+                    createdAt: productVariant.createdAt,
+                    updatedAt: productVariant.updatedAt,
+                },
+            });
+            return right(undefined);
+        } catch (error) {
+            return left(new Error("Failed to create product variant"));
+        }
     }
 
-    const productVariants = variants.map((variant) =>
-      ProductVariant.create({
-        productId: new UniqueEntityID(variant.productId),
-        colorId: variant.colorId
-          ? new UniqueEntityID(variant.colorId)
-          : undefined,
-        sizeId: variant.sizeId ? new UniqueEntityID(variant.sizeId) : undefined,
-        sku: variant.sku,
-        upc: variant.upc ? "" : undefined,
-        stock: variant.stock,
-        price: variant.price,
-        images: variant.images,
-        status: variant.status as ProductStatus,
-        createdAt: new Date(variant.createdAt),
-        updatedAt: variant.updatedAt ? new Date(variant.updatedAt) : undefined,
-      })
-    );
+    async findByProductId(
+        productId: string
+    ): Promise<Either<ResourceNotFoundError, ProductVariant[]>> {
+        console.log("entrou prisma repo findByProductId productId", productId);
 
-    return right(productVariants);
-  }
+        const variants = await this.prisma.productVariant.findMany({
+            where: { productId },
+        });
+        console.log("prisma repo findByProductId variants", variants);
 
-  async findById(
-    id: string
-  ): Promise<Either<ResourceNotFoundError, ProductVariant>> {
-   
-    const variant = await this.prisma.productVariant.findUnique({
-      where: { id },
-    });
- 
-    
-    if (!variant) {
- 
-      return left(
-        new ResourceNotFoundError(
-          `Variant not  in prisma findnyId found for id: ${id}`
-        )
-      );
-    }
+        if (variants.length === 0) {
+            return left(
+                new ResourceNotFoundError(
+                    `Variants not found for product id: ${productId}`
+                )
+            );
+        }
 
-    const productVariant = ProductVariant.create(
-      {
-        productId: new UniqueEntityID(variant.productId),
-        colorId: variant.colorId
-          ? new UniqueEntityID(variant.colorId)
-          : undefined,
-        sizeId: variant.sizeId ? new UniqueEntityID(variant.sizeId) : undefined,
-        sku: variant.sku,
-        upc: variant.upc ?? undefined,
-        stock: variant.stock,
-        price: variant.price,
-        images: variant.images,
-        status: variant.status as ProductStatus,
-        createdAt: new Date(variant.createdAt),
-        updatedAt: variant.updatedAt ? new Date(variant.updatedAt) : undefined,
-      },
-      new UniqueEntityID(variant.id)
-    );
-    
-    return right(productVariant);
-  }
-
-  async findByProductIds(productIds: string[]): Promise<ProductVariant[]> {
-    const variants = await this.prisma.productVariant.findMany({
-      where: {
-        productId: {
-          in: productIds,
-        },
-      },
-    });
-    return variants.map((variant) =>
-      ProductVariant.create(
-        {
-          productId: new UniqueEntityID(variant.productId),
-          sizeId: variant.sizeId
-            ? new UniqueEntityID(variant.sizeId)
-            : undefined,
-          colorId: variant.colorId
-            ? new UniqueEntityID(variant.colorId)
-            : undefined,
-          stock: variant.stock,
-          price: variant.price,
-          status: toDomainProductStatus(variant.status),
-          images: variant.images,
-          createdAt: variant.createdAt,
-        },
-        new UniqueEntityID(variant.id)
-      )
-    );
-  }
-
-  async update(
-    variant: ProductVariant
-  ): Promise<Either<ResourceNotFoundError, void>> {
-    try {
-    
-      const variantIdString = variant.idString;
-   
-      const todasVAriants = await this.prisma.productVariant.findMany();
- 
-
-      const existingVariant = await this.prisma.productVariant.findUnique({
-        where: { id: variantIdString },
-      });
-    
-
-      if (!existingVariant) {
-        return left(
-          new ResourceNotFoundError(
-            `Variant not found for id: ${variant.id.toString()}`
-          )
+        const productVariants = variants.map((variant) =>
+            ProductVariant.create({
+                productId: new UniqueEntityID(variant.productId),
+                colorId: variant.colorId
+                    ? new UniqueEntityID(variant.colorId)
+                    : undefined,
+                sizeId: variant.sizeId
+                    ? new UniqueEntityID(variant.sizeId)
+                    : undefined,
+                sku: variant.sku,
+                upc: variant.upc ? "" : undefined,
+                stock: variant.stock,
+                price: variant.price,
+                images: variant.images,
+                status: variant.status as ProductStatus,
+                createdAt: new Date(variant.createdAt),
+                updatedAt: variant.updatedAt
+                    ? new Date(variant.updatedAt)
+                    : undefined,
+            })
         );
-      }
 
-      await this.prisma.productVariant.update({
-        where: { id: variant.id.toString() },
-        data: {
-          productId: variant.productId.toString(),
-          colorId: variant.colorId?.toString(),
-          sizeId: variant.sizeId?.toString(),
-          sku: variant.sku,
-          upc: variant.upc,
-          stock: variant.stock,
-          price: variant.price,
-          images: variant.images,
-          status: toPrismaProductStatus(variant.status),
-          updatedAt: new Date(),
-        },
-      });
-
-      return right(undefined);
-    } catch (error) {
-      return left(
-        new ResourceNotFoundError(
-          `Failed to update variant with id: ${variant.id.toString()}`
-        )
-      );
+        console.log(
+            "prisma repo findByProductId productVariants",
+            productVariants
+        );
+        return right(productVariants);
     }
-  }
+
+    async findById(
+      
+        id: string
+    ): Promise<Either<ResourceNotFoundError, ProductVariant>> {
+        console.log("entrou prisma repo findByProductId id", id);
+
+        const variant = await this.prisma.productVariant.findFirst({
+            where: { id },
+        });
+        console.log("entrou prisma repo findByProductId variant", variant);
+
+        if (!variant) {
+            return left(
+                new ResourceNotFoundError(
+                    `Variant not  in prisma findnyId found for id: ${id}`
+                )
+            );
+        }
+
+        const productVariant = ProductVariant.create(
+            {
+                productId: new UniqueEntityID(variant.productId),
+                colorId: variant.colorId
+                    ? new UniqueEntityID(variant.colorId)
+                    : undefined,
+                sizeId: variant.sizeId
+                    ? new UniqueEntityID(variant.sizeId)
+                    : undefined,
+                sku: variant.sku,
+                upc: variant.upc ?? undefined,
+                stock: variant.stock,
+                price: variant.price,
+                images: variant.images,
+                status: variant.status as ProductStatus,
+                createdAt: new Date(variant.createdAt),
+                updatedAt: variant.updatedAt
+                    ? new Date(variant.updatedAt)
+                    : undefined,
+            },
+            new UniqueEntityID(variant.id)
+        );
+        console.log(
+            "entrou prisma repo findByProductId productVariant",
+            productVariant
+        );
+
+        return right(productVariant);
+    }
+
+    async findByProductIds(productIds: string[]): Promise<ProductVariant[]> {
+        const variants = await this.prisma.productVariant.findMany({
+            where: {
+                productId: {
+                    in: productIds,
+                },
+            },
+        });
+        return variants.map((variant) =>
+            ProductVariant.create(
+                {
+                    productId: new UniqueEntityID(variant.productId),
+                    sizeId: variant.sizeId
+                        ? new UniqueEntityID(variant.sizeId)
+                        : undefined,
+                    colorId: variant.colorId
+                        ? new UniqueEntityID(variant.colorId)
+                        : undefined,
+                    stock: variant.stock,
+                    price: variant.price,
+                    status: toDomainProductStatus(variant.status),
+                    images: variant.images,
+                    createdAt: variant.createdAt,
+                },
+                new UniqueEntityID(variant.id)
+            )
+        );
+    }
+
+    async update(
+        variant: ProductVariant
+    ): Promise<Either<ResourceNotFoundError, void>> {
+        try {
+            const variantIdString = variant.idString;
+
+            const todasVAriants = await this.prisma.productVariant.findMany();
+
+            const existingVariant = await this.prisma.productVariant.findUnique(
+                {
+                    where: { id: variantIdString },
+                }
+            );
+
+            if (!existingVariant) {
+                return left(
+                    new ResourceNotFoundError(
+                        `Variant not found for id: ${variant.id.toString()}`
+                    )
+                );
+            }
+
+            await this.prisma.productVariant.update({
+                where: { id: variant.id.toString() },
+                data: {
+                    productId: variant.productId.toString(),
+                    colorId: variant.colorId?.toString(),
+                    sizeId: variant.sizeId?.toString(),
+                    sku: variant.sku,
+                    upc: variant.upc,
+                    stock: variant.stock,
+                    price: variant.price,
+                    images: variant.images,
+                    status: toPrismaProductStatus(variant.status),
+                    updatedAt: new Date(),
+                },
+            });
+
+            return right(undefined);
+        } catch (error) {
+            return left(
+                new ResourceNotFoundError(
+                    `Failed to update variant with id: ${variant.id.toString()}`
+                )
+            );
+        }
+    }
 }
