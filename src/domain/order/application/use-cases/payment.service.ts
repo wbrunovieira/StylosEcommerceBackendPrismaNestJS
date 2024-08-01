aa3e4755e16f90189e6841e6b6364b049f39bc85
bruno-minimac <wbrunovieira77@gmail.com>
2024-08-01 08:29:06 -0300

    payment worked

--- src/domain/order/application/use-cases/payment.service.ts
+++ src/domain/order/application/use-cases/payment.service.ts
@@ -1,7 +1,7 @@
 import { Env } from "@/env";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { MercadoPagoConfig, Preference, } from "mercadopago";
+import { MercadoPagoConfig, Preference } from "mercadopago";
 
 interface Item {
     id: string;
@@ -17,43 +17,51 @@ interface Item {
 @Injectable()
 export class MercadoPagoService {
     private client;
-    
+
     constructor(private configService: ConfigService<Env, true>) {
-        const accessToken = configService.get<string>("MERCADO_PAGO_ACCESS_TOKEN");
+        const accessToken = configService.get<string>(
+            "MERCADO_PAGO_ACCESS_TOKEN"
+        );
         this.client = new MercadoPagoConfig({ accessToken });
     }
 
     async createPreference(items: Item[]) {
         try {
-
-            
             const preference = new Preference(this.client);
+
             console.log("payment preference", preference);
 
-            preference
-                .create({
-                    body: {
-                        payment_methods: {
-                            excluded_payment_methods: [
-                                {
-                                    id: "pec",
-                                },
-                            ],
-                            excluded_payment_types: [
-                                {
-                                    id: "debit_card",
-                                },
-                            ],
-                            installments: 3,
-                        },
-                        items: items,
+            const response = await preference.create({
+                body: {
+                    payment_methods: {
+                        excluded_payment_methods: [
+                            {
+                                id: "pec",
+                            },
+                        ],
+                        excluded_payment_types: [
+                            {
+                                id: "debit_card",
+                            },
+                        ],
+                        installments: 3,
                     },
-                })
-                .then(console.log)
-                .catch(console.log);
+                    back_urls: {
+                        success:
+                            "https://wbstylosfrontend.sa.ngrok.io/pagamento-sucesso",
+                        failure:
+                            "https://wbstylosfrontend.sa.ngrok.io/pagamento-falhou",
+                        pending:
+                            "https://wbstylosfrontend.sa.ngrok.io/pagamento-pendente",
+                    },
+                    auto_return: "approved",
+                    items: items,
+                },
+            });
+
             console.log("payment preference create", preference);
-            const response = await this.client.preferences.create(preference);
-            return response.body;
+
+            return response;
         } catch (error) {
             console.log(error);
             throw new Error("Error creating preference");
