dbc979d039dd1d6a1363d5db41842bb15d2063d5
bruno-minimac <wbrunovieira77@gmail.com>
2024-08-09 16:51:39 -0300

    save categories from erp to products from erp

--- src/infra/http/api-erp/fechAllCategoriesFromErp.ts
+++ src/infra/http/api-erp/fechAllCategoriesFromErp.ts
@@ -131,6 +131,7 @@ export class SyncCategoriesUseCase {
                     imageUrl,
                     erpId,
                 };
+                
                 console.log("fetchCategories categoryData", categoryData);
 
                 try {
