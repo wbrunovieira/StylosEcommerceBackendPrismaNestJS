4618be7f1c09c581030ed464bcfafbb4082d88df
bruno-minimac <wbrunovieira77@gmail.com>
2024-08-13 17:09:53 -0300

    migration 58-69

--- src/domain/catalog/application/use-cases/api-all-products.ts
+++ src/domain/catalog/application/use-cases/api-all-products.ts
@@ -23,8 +23,8 @@ export class ApiGetAllProducts {
 
     private readonly token: string;
 
-    private readonly startPage = 70;
-    private readonly endPage = 84;
+    private readonly startPage = 40;
+    private readonly endPage = 57;
 
     constructor(private configService: ConfigService) {
         const token = this.configService.get<string>("TOKEN_CONNECTPLUG");
