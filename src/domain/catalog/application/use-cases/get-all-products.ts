b99a65a954895906a31565b1835901a0fa87fe0c
bruno-minimac <wbrunovieira77@gmail.com>
2024-09-02 11:59:20 -0300

    fixed returns getall products

--- src/domain/catalog/application/use-cases/get-all-products.ts
+++ src/domain/catalog/application/use-cases/get-all-products.ts
@@ -4,13 +4,40 @@ import { IProductRepository } from "../../application/repositories/i-product-rep
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { Injectable } from "@nestjs/common";
 
+export interface ProductObject {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    finalPrice?: number;
+    stock: number;
+    brandName?: string;
+    brandUrl?: string;
+    discount?: number;
+    height: number;
+    width: number;
+    length: number;
+    weight: number;
+    onSale?: boolean;
+    isFeatured?: boolean;
+    isNew?: boolean;
+    images?: string[];
+    slug: string;
+    createdAt: Date;
+    updatedAt: Date;
+    productCategories?: { id: string; name: string }[];
+    productColors?: { id: string; name: string; hex: string }[];
+    productSizes?: { id: string; name: string }[];
+}
 
+
+export type GetAllProductsUseCaseResponse = Either<Error, ProductObject[]>;
 
 @Injectable()
 export class GetAllProductsUseCase {
     constructor(private productRepository: IProductRepository) {}
 
-    async execute(): Promise<Either<Error, Product[]>> {
+    async execute(): Promise<GetAllProductsUseCaseResponse> {
 
         const productsOrError = await this.productRepository.getAllProducts();
 
@@ -23,12 +50,14 @@ export class GetAllProductsUseCase {
         
         const products = productsOrError.value;
 
+        const productsObject = products.map(product => product.toObject());
+
         console.log("GetAllProductsUseCase products", products);
 
         if (products.length === 0) {
             return left(new ResourceNotFoundError("No products found"));
         }
 
-        return right(products);
+        return right(productsObject);
     }
 }
