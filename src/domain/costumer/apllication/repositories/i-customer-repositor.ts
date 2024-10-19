e7c549c4eeb67bdd95f8cc0ef72f6b5524770056
bruno-minimac <wbrunovieira77@gmail.com>
2024-10-19 10:43:10 -0300

    create order and customer when is paid is aproved

--- src/domain/costumer/apllication/repositories/i-customer-repositor.ts
+++ src/domain/costumer/apllication/repositories/i-customer-repositor.ts
@@ -8,4 +8,5 @@ export abstract class ICustomerRepository {
     abstract findCustomerById(customerId: string): Promise<Either<Error, Customer>>;
     abstract create(customer: Customer): Promise<Either<Error, void>>;
     abstract listAllCustomers(): Promise<Either<Error, Customer[]>>;
+    abstract findByUserId(userId: string): Promise<Either<Error, Customer | null>>
 }
