cc9e503e3c5f8fbb986cae3e67893b4bcaadcb00
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-11-16 17:56:54 -0300

    refactored separreted versions prod and dev

--- load_env.sh
+++ load_env.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
-export $(grep -v '^#' /app/.env | xargs)
+
+
+if [ "$NODE_ENV" = "production" ]; then
+  ENV_FILE="/app/.env.production"
+elif [ "$NODE_ENV" = "development" ]; then
+  if [ -f "/app/.env.development.local" ]; then
+    ENV_FILE="/app/.env.development.local"
+  else
+    ENV_FILE="/app/.env.development"
+  fi
+elif [ "$NODE_ENV" = "test" ]; then
+  ENV_FILE="/app/.env.test"
+else
+  echo "Ambiente não reconhecido. Usando o arquivo padrão."
+
+fi
+
+echo "Carregando variáveis de ambiente do arquivo: $ENV_FILE"
+
+# Exportar as variáveis do arquivo escolhido
+export $(grep -v '^#' "$ENV_FILE" | xargs)
+
+# Executar o comando original
 exec "$@"
