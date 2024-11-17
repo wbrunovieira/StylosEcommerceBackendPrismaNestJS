003387be24beab01f28b65404156a728ad55620a
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-11-17 15:09:28 -0300

    divide Dockerfile in two stages

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,17 +10,16 @@ resource "aws_instance" "stylos_ec2" {
   tags = {
     Name = "stylos-ec2-instance"
   }
+   root_block_device {
+    volume_size = 20 
+    volume_type = "gp3"  
+  }
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo mkfs.ext4 /dev/xvdf
-    sudo mkdir -p /mnt/data
-    sudo mount /dev/xvdf /mnt/data
-    echo "/dev/xvdf /mnt/data ext4 defaults 0 0" | sudo tee -a /etc/fstab
-  EOF
+
 }
 
 resource "aws_eip_association" "stylos_eip" {
   instance_id   = aws_instance.stylos_ec2.id
   allocation_id = "eipalloc-0016ff067468c6caa"
 }
+
